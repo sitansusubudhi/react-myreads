c2abca2a714b46cbb47b992456c64cd371bfd17b
Sitansu Subudhi <sitansu.subudhi@outlook.com>
2019-11-08 09:51:52 +0530

    feat: Configure BooksAPI to get and update data in main page

--- src/Book.js
+++ src/Book.js
@@ -9,7 +9,7 @@ const Book = (props) => {
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
                     <div className="book-shelf-changer">
-                        <select value={book.shelf} onChange={(event) => onChangeSelectOption(book.id, event.target.value)}>
+                        <select value={book.shelf} onChange={(event) => onChangeSelectOption(book, event.target.value)}>
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Book from './Book';
 
 const BookShelf = (props) => {
-    const { books, shelfTitle, onChangeBookShelf } = props;
+    const { books, shelf, shelfTitle, onChangeBookShelf } = props;
+    const shelfBooks = books.filter((book) => (shelf.includes(book.id)));
+    
+    console.log(shelfBooks);
 
     return (
         <div className="bookshelf">
             <h2 className="bookshelf-title">{shelfTitle}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    {books.map((book) => (
+                    {shelfBooks.map((book) => (
                         <Book key={book.id} book={book} onChangeSelectOption={onChangeBookShelf} />
                     ))}
                 </ol>
