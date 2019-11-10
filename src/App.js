96eee64614e7b455cf937a9593879af1fa4a3cce
Sitansu Subudhi <sitansu.subudhi@outlook.com>
2019-11-10 20:34:44 +0530

    docs: Following the Udacity style guide for App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
-import BookShelves from './BookShelves'
-import SearchBooks from './SearchBooks'
-import { Route } from 'react-router-dom'
+import React from 'react';
+import './App.css';
+import BookShelves from './BookShelves';
+import SearchBooks from './SearchBooks';
+import { Route } from 'react-router-dom';
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+class App extends React.Component {
 
   render() {
     return (
       <div className="app">
+        {/**
+         * Since, we do not need to pass props to the child components,
+         * we can let react router to render the component based on the URL path.
+         */} 
         <Route path="/search" component={SearchBooks} />
         <Route exact path="/" component={BookShelves} />
       </div>
@@ -26,4 +20,4 @@ class BooksApp extends React.Component {
   }
 }
 
-export default BooksApp
+export default App;
