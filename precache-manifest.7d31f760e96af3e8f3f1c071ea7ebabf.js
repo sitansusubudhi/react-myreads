f685626432fe6f5049f461a6e53580170d90da1d
Sitansu Subudhi <sitansu.subudhi@outlook.com>
2020-03-17 09:23:08 +0530

    Updates

---
+++ precache-manifest.7d31f760e96af3e8f3f1c071ea7ebabf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/React-MyReads/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6e620cf0cb74bc9c15be38343e953f04",
+    "url": "/React-MyReads/static/media/dual-ring.6e620cf0.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/React-MyReads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/React-MyReads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/React-MyReads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "dbbf21e1a55c4d0ae7e1",
+    "url": "/React-MyReads/static/js/runtime~main.dbbf21e1.js"
+  },
+  {
+    "revision": "c259d78da90b6e3e6098",
+    "url": "/React-MyReads/static/js/main.c259d78d.chunk.js"
+  },
+  {
+    "revision": "7a1d62fbbd186a7aab88",
+    "url": "/React-MyReads/static/js/1.7a1d62fb.chunk.js"
+  },
+  {
+    "revision": "c259d78da90b6e3e6098",
+    "url": "/React-MyReads/static/css/main.846ebad6.chunk.css"
+  },
+  {
+    "revision": "42046efc00359ffda7a00eae8a56d833",
+    "url": "/React-MyReads/index.html"
+  }
+];
